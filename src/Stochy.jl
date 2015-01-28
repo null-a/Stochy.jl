22d51459804fbff4650aca854660b609ac52cf34
null-a <null-a@users.noreply.github.com>
2015-01-28 16:00:37 +0000

    Add square root primitive.

--- src/Stochy.jl
+++ src/Stochy.jl
@@ -10,7 +10,7 @@ const score = Distributions.logpdf
 export @pp, sample, factor, score, observe, mem
 export Discrete, Dir, flip, randominteger, hellingerdistance
 
-const primitives = [:+,:*,:-,:/,
+const primitives = [:+,:*,:-,:/,:sqrt,:√,
                     :tuple, :mem, :println, :cons, :list, :tail, :cat,
                     :reverse, :.., :first, :second, :third, :fourth]
 
