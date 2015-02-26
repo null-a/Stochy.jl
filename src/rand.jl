7103dded7222d185612cea0f6154fb3b9c742586
null-a <null-a@users.noreply.github.com>
2015-02-26 16:01:26 +0000

    Fix bug in PMCMC when using a single particle.
    If the single particle has a weight of -Inf an error is now raised.
    Previously this was ignored resulting in samples from the wrong
    distribution.

--- src/rand.jl
+++ src/rand.jl
@@ -5,8 +5,11 @@ function rand(ps::Vector{Float64})
     n = length(ps)
     for i in 1:n-1
         @inbounds acc += ps[i]
-        isnan(acc) && error("unexpected nan in rand")
+        isnan(acc) && nanerror()
         r < acc && return i
     end
+    n == 1 && isnan(ps[1]) && nanerror()
     n
 end
+
+nanerror() = error("unexpected nan in rand")
--- test/randtests.jl
+++ test/randtests.jl
@@ -1,1 +1,2 @@
+@test_throws ErrorException Stochy.rand([NaN])
 @test_throws ErrorException Stochy.rand([NaN, NaN])
